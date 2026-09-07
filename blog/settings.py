d155c6c2a9381c75a03839a44ba1cf2e7386dddb
MD.Abu Ayub Bhuyan <abuayubminhaz290@gmail.com>
2026-09-07 17:54:58 +0600

    Second FaceBook

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jvhrocu02+!7!t$5j=59#pz0atp@6*qk(qf85!0q5z%8-$jl8c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
